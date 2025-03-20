d66f6bbc9410474278c04e16994ae70469eb9023
timsu92 <33785401+timsu92@users.noreply.github.com>
2025-03-21 00:24:16 +0800

    feat(dataset): load data and do transforms

--- src/oxford_pet.py
+++ src/oxford_pet.py
@@ -1,11 +1,15 @@
 import os
-import torch
 import shutil
-import numpy as np
+from typing import Literal
 
+import numpy as np
+import torch
+from torchvision.transforms import v2 as transforms
+from torchvision import tv_tensors
 from PIL import Image
 from tqdm import tqdm
 from urllib.request import urlretrieve
+
 
 class OxfordPetDataset(torch.utils.data.Dataset):
     def __init__(self, root, mode="train", transform=None):
@@ -30,22 +34,24 @@ class OxfordPetDataset(torch.utils.data.Dataset):
         image_path = os.path.join(self.images_directory, filename + ".jpg")
         mask_path = os.path.join(self.masks_directory, filename + ".png")
 
-        image = np.array(Image.open(image_path).convert("RGB"))
+        image = tv_tensors.Image(Image.open(image_path))
 
         trimap = np.array(Image.open(mask_path))
         mask = self._preprocess_mask(trimap)
+        mask = tv_tensors.Mask(mask)
 
         sample = dict(image=image, mask=mask, trimap=trimap)
         if self.transform is not None:
-            sample = self.transform(**sample)
+            sample = self.transform(sample)
 
         return sample
 
     @staticmethod
     def _preprocess_mask(mask):
-        mask = mask.astype(np.float32)
-        mask[mask == 2.0] = 0.0
-        mask[(mask == 1.0) | (mask == 3.0)] = 1.0
+        # Original: 1: Foreground 2:Background 3: Not classified xmls/ Head bounding box
+        # Modified: 0: Background 1: Foreground & bounding box
+        mask[mask == 2] = 0
+        mask[(mask == 1) | (mask == 3)] = 1
         return mask
 
     def _read_split(self):
@@ -54,10 +60,12 @@ class OxfordPetDataset(torch.utils.data.Dataset):
         with open(split_filepath) as f:
             split_data = f.read().strip("\n").split("\n")
         filenames = [x.split(" ")[0] for x in split_data]
-        if self.mode == "train":  # 90% for train
-            filenames = [x for i, x in enumerate(filenames) if i % 10 != 0]
-        elif self.mode == "valid":  # 10% for validation
-            filenames = [x for i, x in enumerate(filenames) if i % 10 == 0]
+        # train:validation = 8:1
+        if self.mode == "train":
+            filenames = [x for i, x in enumerate(filenames) if i % 9 != 0]
+        elif self.mode == "valid":
+            filenames = [x for i, x in enumerate(filenames) if i % 9 == 0]
+        # return all for tests?
         return filenames
 
     @staticmethod
@@ -78,24 +86,6 @@ class OxfordPetDataset(torch.utils.data.Dataset):
             filepath=filepath,
         )
         extract_archive(filepath)
-
-
-class SimpleOxfordPetDataset(OxfordPetDataset):
-    def __getitem__(self, *args, **kwargs):
-
-        sample = super().__getitem__(*args, **kwargs)
-
-        # resize images
-        image = np.array(Image.fromarray(sample["image"]).resize((256, 256), Image.BILINEAR))
-        mask = np.array(Image.fromarray(sample["mask"]).resize((256, 256), Image.NEAREST))
-        trimap = np.array(Image.fromarray(sample["trimap"]).resize((256, 256), Image.NEAREST))
-
-        # convert to other format HWC -> CHW
-        sample["image"] = np.moveaxis(image, -1, 0)
-        sample["mask"] = np.expand_dims(mask, 0)
-        sample["trimap"] = np.expand_dims(trimap, 0)
-
-        return sample
 
 
 class TqdmUpTo(tqdm):
@@ -128,7 +118,34 @@ def extract_archive(filepath):
     if not os.path.exists(dst_dir):
         shutil.unpack_archive(filepath, extract_dir)
 
-def load_dataset(data_path, mode):
-    # implement the load dataset function here
 
-    assert False, "Not implemented yet!"
+def load_dataset(
+    data_path="./dataset/oxford-iiit-pet",
+    mode: Literal["train", "valid", "test"] = "train",
+):
+    transform_train = [
+        transforms.ToImage(),
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomRotation(20),
+        transforms.RandomResizedCrop(
+            (224, 224), scale=(0.64, 1)
+        ),  # TODO: what's the input size for ResNet34?
+        transforms.RandomPerspective(0.35, 0.7),
+        transforms.ColorJitter(brightness=0.5, contrast=0.2, saturation=0.4, hue=0.1),
+        transforms.ToDtype(torch.float32, scale=True),
+    ]
+
+    transform_test = [
+        transforms.ToImage(),
+        transforms.Resize((224, 224)),
+        transforms.ToDtype(torch.float32, scale=True),
+    ]
+
+    if mode != "test":
+        transform = transforms.Compose(transform_train)
+    else:
+        transform = transforms.Compose(transform_test)
+
+    dataset = OxfordPetDataset(data_path, mode=mode, transform=transform)
+
+    return dataset
