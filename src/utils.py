3faf94af22798f2f1481554fbee70a8d65877c1f
timsu92 <33785401+timsu92@users.noreply.github.com>
2025-03-22 14:13:05 +0800

    feat(utils): implement dice score

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,8 @@
-def dice_score(pred_mask, gt_mask):
-    # implement the Dice score here
-    
-    assert False, "Not implemented yet!"
+import torch
+
+
+def dice_score(pred_mask: torch.Tensor, gt_mask: torch.Tensor):
+    # turn into uint8 for bitwise operations
+    pred_mask = pred_mask.to(torch.uint8)
+    gt_mask = gt_mask.to(torch.uint8)
+    return 2 * (pred_mask & gt_mask).sum() / (pred_mask.numel() + gt_mask.numel())
