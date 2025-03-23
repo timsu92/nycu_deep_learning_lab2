e8ad5d72c64cd2b3a245f9fb250b83acb1a5502d
timsu92 <33785401+timsu92@users.noreply.github.com>
2025-03-23 19:02:59 +0800

    fix(dice): previous method is wrong

--- src/utils.py
+++ src/utils.py
@@ -1,11 +1,18 @@
 import torch
 
 
-def dice_score(pred_mask: torch.Tensor, gt_mask: torch.Tensor):
+# https://chih-sheng-huang821.medium.com/%E5%BD%B1%E5%83%8F%E5%88%87%E5%89%B2%E4%BB%BB%E5%8B%99%E5%B8%B8%E7%94%A8%E7%9A%84%E6%8C%87%E6%A8%99-iou%E5%92%8Cdice-coefficient-3fcc1a89cd1c
+def dice_score(pred_mask: torch.Tensor, gt_mask: torch.Tensor, epsilon: float = 1e-6):
     # turn into uint8 for bitwise operations
     pred_mask = pred_mask.to(torch.uint8)
     gt_mask = gt_mask.to(torch.uint8)
-    return 2 * (pred_mask & gt_mask).sum() / (pred_mask.numel() + gt_mask.numel())
+    dim = (-1, -2, -3)
+    inter = 2 * (pred_mask & gt_mask).sum(dim=dim)
+    sets_sum = pred_mask.sum(dim=dim) + gt_mask.sum(dim=dim)
+    sets_sum = torch.where(sets_sum == 0, inter, sets_sum)
+
+    dice = (inter + epsilon) / (sets_sum + epsilon)
+    return dice.mean()
 
 # https://github.com/milesial/Pytorch-UNet/blob/master/utils/dice_score.py
 def dice_loss(pred_mask: torch.Tensor, gt_mask: torch.Tensor):
